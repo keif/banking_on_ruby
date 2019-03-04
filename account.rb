57817368bc938ab50a5ad294cf74286cf1ce65a8
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-03-04 01:08:01 -0500

    init account class

---
+++ account.rb
@@ -0,0 +1,16 @@
+class Account
+    attr_reader :name
+    attr_reader :balance
+    def initialize(name, balance = 100)
+      @name = name
+      @balance = balance
+    end
+    
+    private
+    def pin
+      @pin = 1234
+    end
+    def pin_error
+      "Access denied: incorrect PIN."
+    end
+  end
